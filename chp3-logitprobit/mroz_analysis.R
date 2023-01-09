716eb797c83deb47523b1efdeca5c485b6f4587e
gaboruh <gaboruh@gmail.com>
2023-01-09 12:59:45 +0100

    Calculate marginal effects manually vs mfx

--- chp3-logitprobit/mroz_analysis.R
+++ chp3-logitprobit/mroz_analysis.R
@@ -34,3 +34,37 @@ logitmfx(model_formula, data = mroz)
 probitmfx(model_formula, data = mroz)
 
 summary(reg_logit)
+
+#Manually calculating "marginal effects" (average partial effects)
+reg_logit <- glm(model_formula, family=binomial(link="logit"), data = mroz)
+summary(reg_logit)
+
+#nwifeinc + educ + exper + expersq + age + kidslt6 + kidsge6
+
+Xmat <- cbind(1,mroz$nwifeinc, mroz$educ, mroz$exper, mroz$expersq, mroz$age, mroz$kidslt6, mroz$kidsge6)
+
+Xmat
+
+xpb <- Xmat %*% reg_logit$coefficients
+
+gdot <- rep(NA, dim(Xmat)[1])
+
+for (i in 1:dim(Xmat)[1]) {
+gdot[i] <- dlogis(xpb[i]) 
+}
+
+#This is what mfx package calculates
+xave <- 1/dim(Xmat)[1] * colSums(Xmat)
+
+dlogis(xave %*% reg_logit$coefficients) * reg_logit$coefficients[3]
+
+
+#This is what I would like it to calculate (APE)
+
+gdot[70] * reg_logit$coefficients[3]
+
+mean(gdot) *reg_logit$coefficients[3]
+
+
+
+logitmfx(model_formula, data =mroz)
