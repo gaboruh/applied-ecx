2f8cea90d67a6793a35c6b39b09d3c96e84fb711
gaboruh <gaboruh@gmail.com>
2023-01-23 15:04:28 +0100

    estimate arima on two time series

--- chp4-timeseries/elecequip_analysis.R
+++ chp4-timeseries/elecequip_analysis.R
@@ -11,11 +11,57 @@ plot(decompose(elecequip, type="additive"))
 plot(stl(elecequip, s.window="periodic"))
 
 ts_remainder <- decompose(elecequip, type="additive")$random
+anyNA(ts_remainder)
+
+ts_remainder <- ts_remainder[!is.na(ts_remainder)]
 
 autoplot(ts_remainder)
 
 #Unit root testing.
 
+library(urca)
+
+PP.test(ts_remainder)
+summary(ur.kpss(ts_remainder))
+library(tseries)
+
+kpss.test(ts_remainder)
+adf.test(ts_remainder)
+
+dts <- diff(ts_remainder)
+anyNA(dts)
+autoplot(dts)
+
+plot(dts, type="l")
+
+kpss.test(dts)
+
+library(data.table)
+#sp500 <- fread("SP500.csv")
+#plot(sp500)
+#decompose(sp500)
+#library(xts)
+#as.xts(sp500)
+#library(fpp3)
+#library(forecast)
+summary(arma(ts_remainder)) # AIC is 866
+
+summary(arma(ts_remainder, order=c(1,2))) #AIC is larger! ARMA(1,2)
+
+summary(arma(ts_remainder, order=c(1,0))) #ARMA(1,0)
+
+summary(arma(ts_remainder, order=c(0,1))) #ARMA(0,1)
+
+#ARMA(0,0) white noise
+
+library(fpp2)
+
+auto.arima(ts_remainder)
+
+auto.arima(sp500[,2]) #AICC criterion to select lag order; 
+# uses the KPSS test to test stationarity;
+# treats missing values automatically. Kalman filter methods to impute missing values.
+plot(forecast(auto.arima(sp500[,2])))
 
 
          
